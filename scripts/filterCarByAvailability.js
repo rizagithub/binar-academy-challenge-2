22eff122245f65292d14de9f617d8b2b25c9bf0b
rizagithub <rizatul.20004@mhs.unesa.ac.id>
2023-03-17 13:24:11 +0700

    first commit

--- scripts/filterCarByAvailability.js
+++ scripts/filterCarByAvailability.js
@@ -6,7 +6,17 @@ function filterCarByAvailability(cars) {
   const result = [];
 
   // Tulis code-mu disini
+  for (let i = 0; i < cars.length; i++) {
+    if (cars[i].available == true) {
+      result.push(cars[i]);
+    }
+  }
+  
+  // menggunakan array method
+  // const filteredCars = result.filter((a) => a.available == true);
+  // console.table(filteredCars);
 
   // Rubah code ini dengan array hasil filter berdasarkan availablity
+  console.log(result);
   return result;
 }
--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -7,6 +7,20 @@ function sortCarByYearAscendingly(cars) {
   const result = [...cars];
 
   // Tulis code-mu disini
+  for(let i = 1; i < result.length; i++) {
+    let j = i;
+    while (j > 0  && result[j - 1].year > result[j].year) {
+      // temp digunakan sebagai wadah menempatkan value result[j]
+      let temp = result[j - 1];
+      result[j - 1] = result[j];
+      result[j] = temp;
+      j--;
+    }
+  }
+
+  // menggunakan array method sort
+  // result.sort((a, b) => a.year - b.year);
+  // console.table(result);
 
   // Rubah code ini dengan array hasil sorting secara ascending
   return result;
